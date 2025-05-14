48dec928151de008f5702bb230882341a5bca4df
alancraju <146972696+alancraju@users.noreply.github.com>
2025-05-14 22:07:01 +0530

    Update pr10.java

--- pr10.java
+++ pr10.java
@@ -1,4 +1,4 @@
-// Write a Java program that swaps two elements ( first and third elements ) in a linked list ( using
+//10. Write a Java program that swaps two elements ( first and third elements ) in a linked list ( using
 // Collections.swap(l_list, 0, 2))
 
 
